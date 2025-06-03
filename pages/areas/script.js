231fa68cd0f0b45ab1dd27b44e3fb4d0cc58cd6a
Fulano de Tal <fulanodetal@exemplo.br>
2025-06-03 17:15:26 -0300

    aula 03.06

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', async function(){
-    const area = "Mistieri";
-    const tokem = localStorage.getItem('tokem');
-    const response = await fetch(`http://localhost:3000/brinquedos/${area}`, {
+    const urlParams = new URLSearchParams(window.location.search);
+    const area = urlParams.get('area');
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+    const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + token
         },
-        authorization: "Bearer" + tokem
+
     });
 
     const data = await response.json();
     console.log(data);
+
+    for (var brinquedos of data.resultados) {
+        console.log(brinquedos);
+
+        const grid = document.querySelector(".rides-grid");
+        grid.innerHTML = grid.innerHTML + `
+        <div class="ride-card">
+            <div class="ride-image" style="background-image: url('../others/img/Katapul.jpg')"></div>
+            <div class="ride-info">
+               <h3 class="ride-name">${brinquedos.name}</h3>
+               <div class="ride-time">${brinquedos.waiting_time}</div>
+               <span class="ride-status status-busy">${brinquedos.status}</span>
+            </div>
+        </div>`
+    }
 });
